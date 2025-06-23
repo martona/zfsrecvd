3c6498b4a2a4f29ac7ba4dd37e5a6b97f5998260
marton <marton@anka.me>
2025-06-22 20:12:05 -0400

    allow non-raw sends for unencrypted datasets

--- send.sh
+++ send.sh
@@ -180,19 +180,35 @@ for (( idx=${#local_prior[@]}-1; idx>=0; idx-- )); do
 done
 
 #
+# ---------- 9.  decide raw mode ----------------------------------------------
+#
+
+# Find the encryption status of the dataset that this one derives its 
+# encryption from. If the dataset is encrypted, we need to send it raw.
+
+enc_root=$(zfs get -H -o value encryptionroot "$dataset")
+need_raw=false
+if [[ "$enc_root" != "-" ]]; then               # "-" means dataset not encrypted
+    enc_prop=$(zfs get -H -o value encryption "$enc_root")
+    [[ "$enc_prop" != "off" ]] && need_raw=true
+fi
+
+SEND_RAW=$([[ $need_raw == true ]] && echo "-w" || echo "")
+
+#
 # ---------- 9.  ship the stream ---------------------------------------------
 #
 if [[ -n "$common" ]]; then
     echo "Sending incremental from [${$dataset}@${common}] to [${full_snap}]" >&2
     # determine size of the incremental send
-    size=$( zfs send -nP wi "${dataset}@${common}" "${full_snap}" | awk '/^size/{print $2;exit}' )
+    size=$( zfs send -nP $SEND_RAW -i "${dataset}@${common}" "${full_snap}" | awk '/^size/{print $2;exit}' )
     # Incremental: -w (raw), -i FROM@ TO@
-    zfs send -wi "${dataset}@${common}" "${full_snap}" | pv "$PV_FORCE_FLAG" ${size:+-s "$size"} >&${OUT}
+    zfs send $SEND_RAW -i "${dataset}@${common}" "${full_snap}" | pv "$PV_FORCE_FLAG" ${size:+-s "$size"} >&${OUT}
 else
     echo "No common snapshot; full send: [${full_snap}]" >&2
     # determine size
-    size=$( zfs send -nP -w "${full_snap}" 2>&1 | awk '/^size/{print $2;exit}' )
-    zfs send -w "${full_snap}" | pv "$PV_FORCE_FLAG" ${size:+-s "$size"} >&${OUT}
+    size=$( zfs send -nP $SEND_RAW "${full_snap}" 2>&1 | awk '/^size/{print $2;exit}' )
+    zfs send $SEND_RAW "${full_snap}" | pv "$PV_FORCE_FLAG" ${size:+-s "$size"} >&${OUT}
 fi
 echo "Send successful." >&2
 
